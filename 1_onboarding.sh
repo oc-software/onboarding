0857cf24cb1f66c352c2d2c8e1c983077359ef83
oc-software <104031098+oc-software@users.noreply.github.com>
2022-07-06 15:21:06 -0600

    Update 1_onboarding.sh

--- 1_onboarding.sh
+++ 1_onboarding.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo -e "\nRunning script...\n"
-sleep 10
+###     Mac onboarding script for phase 1       ###
+
 echo -e "\nPlease type the computer name:\n"
 read compName
-echo $compName
+scutil --set HostName "$compName"
+scutil --set LocalHostName "$compName"
+scutil --set ComputerName "$compName"
+echo -e "\nComputer name set to $compName"
+dscacheutil -flushcache
+echo -e "\nDNS cache flushed. Rebooting in 10 seconds...\n"
+sleep 10 ; reboot
