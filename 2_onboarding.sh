d9afb3343892316af6e018f4a765a075a50e44a3
oc-software <104031098+oc-software@users.noreply.github.com>
2022-12-06 12:55:17 -0700

    Streamlined user creation

--- 2_onboarding.sh
+++ 2_onboarding.sh
@@ -5,29 +5,27 @@ jamf recon
 read -n 1 -srp "Press any key to continue once you are done checking Jamf, or ctrl+c to exit"
 jamf policy
 
-
 echo -e "\nPlease type the new user's username(Do not use quotes):\n"
-read username
-echo -e "\nPlease type the new user's full name(Do not use quotes):\n"
-read fullname
+read -r username
 
-dscl . -create /Users/"$username"
-dscl . -create /Users/"$username" RealName "$fullname"
-dscl . -create /Users/"$username" UserShell /bin/zsh
-dscl . -create /Users/"$username" NFSHomeDirectory /Users/"$username"
-dscl . -create /Users/"$username" UniqueID 502
-dscl . -create /Users/"$username" PrimaryGroupID 20
-echo -e "\nPlease type a temporary password for the new user(Do not use quotes)\n"
-read pass
-dscl . -passwd /Users/"$username" "$pass"
+echo -e "\nPlease type a temporary password for the new user(Do not use quotes):\n"
+read -r pass
 
-read -p "Is this user an administrator? [y/n]" continue
+read -r -p "Is this user an administrator? [y/n]" continue
 echo
 if [ "$continue" == "y" ]
 then
-    dscl . -append /Groups/admin GroupMembership "$username"
-    echo -e "\n\n\n!! User added as admin, Please reboot computer !! \n\n\n"
+    admin="$username.admin"
+    sysadminctl -addUser "$username" -password "$pass"
+
+    echo -e "\nPlease type a temporary password for the user's admin account (Do not use quotes):\n"
+    read -r adminPass
+    sysadminctl -addUser "$admin" -password "$adminPass" -admin
+
+    echo -e "\n$username can now be used to log in and $admin can be used to perform administrative functions. Make sure the user understands that they should NEVER log in with $admin\n"
+    echo -e "\n\n\n!! Please reboot computer !! \n\n\n"
 else
+    sysadminctl -addUser "$username" -password "$pass"
     echo -e "\nComplete!\n"
     echo -e "\n\n\n!! Please reboot computer !! \n\n\n"
     exit
