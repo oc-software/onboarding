8cffe29a3eb550a641d05713d81a88949be56f35
oc-software <104031098+oc-software@users.noreply.github.com>
2022-12-06 13:40:58 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -n 1 -srp "Press any key to continue or ctrl+c to exit"
+
+echo -e "\nPlease enter a test string\n"
+read -r test
+echo -e "\n$test\n"
+read -r -p "\nDo you like ducks? [y/n]" continue
+echo
+if [[ "$continue" == "y" ]]; then
+  echo "\nGood...\n"
+ else
+  echo "\nHmmmm....\n"
+ fi
